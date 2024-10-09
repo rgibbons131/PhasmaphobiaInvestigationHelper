dbf9c2f5b19ca887534b0e1f767bfed1a00a7689
rgibbons131 <richiegibbons99@gmail.com>
2024-10-09 13:58:38 -0600

    Checkboxes abstracted

--- src/app/triStateCheckbox.js
+++ src/app/triStateCheckbox.js
@@ -1,3 +1,10 @@
-export default function TriStateCheckbox() {
-  return;
+export default function TriStateCheckbox({ name, valueIn = name }) {
+  return (
+    <li>
+      <label>
+        <input type="checkbox" id={valueIn} name={valueIn} value={valueIn} />
+        {name}
+      </label>
+    </li>
+  );
 }
