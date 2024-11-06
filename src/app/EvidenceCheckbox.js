2e407140de6d20a22a9e716f0c3d9003f6878bf6
rgibbons131 <richiegibbons99@gmail.com>
2024-11-06 16:14:37 -0700

    Visual Update
    Maps added to repo, start working on different pages, ghost content modularized, checkboxes placed instead of placeholder text

--- src/app/EvidenceCheckbox.js
+++ src/app/EvidenceCheckbox.js
@@ -4,7 +4,7 @@ export default function EvidenceCheckbox({ name, valueIn = name, val, func }) {
   return (
     <li className={styles.evidenceCheckbox} onClick={func}>
       <div id={valueIn} name={valueIn}>
-        {name}, {val ? "text" : "empty"}
+        {val ? "\u2611" : "\u2610"} {name}
       </div>
     </li>
   );
